1cae5916d923472771d3b6e975729ae1c6a0b6d5
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-05-28 21:57:48 +0500

    if else

---
+++ JS Coditionals/div_by_2_and_3.js
@@ -0,0 +1,8 @@
+let number = 10;
+
+if(number % 2 == 0  && number % 3 == 0){
+    console.log("Number is divisible by 2 and 3")
+}
+else{
+    console.log("Number is not divisible by 2 and ")
+}
---
+++ JS Coditionals/div_by_2_or_3.js
@@ -0,0 +1,8 @@
+let number = 10;
+
+if(number % 2 == 0  ||  number % 3 == 0){
+    console.log("Number is divisible by either 2 or 3")
+}
+else{
+    console.log("Number is not divisible by 2 or 3 ")
+}
