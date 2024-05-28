e1a700ec902a382c497f9ee4db1d5ef06f67ccdf
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-05-28 20:05:43 +0500

    Initial commit

---
+++ Intro to JS/script.js
@@ -0,0 +1,18 @@
+alert("Hello World")
+
+console.log("Console is running...")
+console.log("Console is also running...")
+console.log("Console is running like a wow...")
+
+var a = prompt("Enter your number ")
+
+var isTrue = confirm("Are you sure you want to leave this site? ")
+
+if (isTrue) {
+    console.log("leaving the site.")
+}
+else {
+
+    console.log("leaving the site.")
+}
+console.log(a)
