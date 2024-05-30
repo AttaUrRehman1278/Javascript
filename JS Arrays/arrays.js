2d671bde2fbb32766e3d9e240251cea6701db04a
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-05-30 18:35:47 +0500

    Arrays

--- JS Arrays/arrays.js
+++ JS Arrays/arrays.js
@@ -1,7 +1,13 @@
-const prompt = require("prompt-sync")();
-let a = [];
-do {
-    var b = parseInt(prompt("Enter a number: "))
-    a.push(b)
-} while(b != 0);
-console.log(a)
+// const prompt = require("prompt-sync")();
+// let a = [];
+// do {
+//     var b = parseInt(prompt("Enter a number: "))
+//     a.push(b)
+// } while(b != 0);
+// console.log(a)
+
+let a = [1, 3, 5, 4];
+b = a.map(x =>{
+    return x ** 2;
+})
+console.log(b)
