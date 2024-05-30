0e636c39e7e90a3ab0497a164ef1c91198889f17
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-05-30 22:49:54 +0500

    Factorial

--- Exercise 11/factorial.js
+++ Exercise 11/factorial.js
@@ -2,13 +2,30 @@ const prompt = require("prompt-sync")();
 
 let a = parseInt(prompt('Enter a number: '));
 
-function factorial(x){
-    if(x == 0 || x == 1){
-        return 1;
+/* Finding factorial using recursion method
+
+// function factorial(x){
+//     if(x == 0 || x == 1){
+//         return 1;
+//     }
+//     else{
+//         return x * factorial(x-1);
+//     }
+// }
+// console.log(factorial(a))
+
+*/
+
+/* FInding factorial using for loop
+
+*/
+let fact = a;
+for (let i = a; i > 0; i--){
+    if(i == 1){
+        fact = fact * 1;
     }
     else{
-        return x * factorial(x-1);
+        fact = fact * (i - 1);
     }
 }
-
-console.log(factorial(a))
+console.log(fact)
