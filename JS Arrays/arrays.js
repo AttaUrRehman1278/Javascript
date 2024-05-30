081494d92d2dc6041846bc38929a55a1ace52d59
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-05-30 18:48:18 +0500

    Arrays

--- JS Arrays/arrays.js
+++ JS Arrays/arrays.js
@@ -6,9 +6,21 @@
 // } while(b != 0);
 // console.log(a)
 
-let a = [1, 2, 3, 4, 5];
+// let a = [1, 2, 3, 4, 5];
 
-let b = a.map(e => {
-    return e**3
-})
-console.log(b)
+// let b = a.map(e => {
+//     return e**3
+// })
+// console.log(b)
+
+
+let a = [10, 30, 345, 23, 5];
+
+const divisibleByTen = (e) => {
+    if (e % 10 == 0){
+        return true;
+    }
+    return false;
+}
+
+console.log(a.filter(divisibleByTen));
