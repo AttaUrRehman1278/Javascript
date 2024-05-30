053767f58c44fc16f861b19de18d26dae48f0e9b
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-05-30 22:13:26 +0500

    Exercise 11

---
+++ Exercise 11/factorial.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")();
+
+let a = parseInt(prompt('Enter a number: '));
+
+function factorial(x){
+    if(x == 0 || x == 1){
+        return 1;
+    }
+    else{
+        return x * factorial(x-1);
+    }
+}
+
+console.log(factorial(a))
