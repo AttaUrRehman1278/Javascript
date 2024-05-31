a6a5099b7dd46f02cf90f391824849a10689af3e
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-05-31 18:52:52 +0500

    Dom

--- JS DOM/script.js
+++ JS DOM/script.js
@@ -1,19 +1,28 @@
-let a = confirm("Do you want to see prompt?")
-if (a == true){
+// let a = confirm("Do you want to see prompt?")
+// if (a == true){
 
-    let age = prompt("Enter your age: ");
+//     let age = prompt("Enter your age: ");
     
-    if (age > 4){
-        // alert("You can drive!")
-        // location.href = "https://google.com";
-        console.log(typeof(document))
-        console.log(typeof(window))
+//     if (age > 4){
+//         // alert("You can drive!")
+//         // location.href = "https://google.com";
+//         console.log(typeof(document))
+//         console.log(typeof(window))
 
-    }
-    else if (age < 0){
-        console.error("Wrong Input")
-    }
-    else{
-        alert("You are underaged. You can't drive!")
-    }
-}
+//     }
+//     else if (age < 0){
+//         console.error("Wrong Input")
+//     }
+//     else{
+//         alert("You are underaged. You can't drive!")
+//     }
+// }
+
+// let a = document.body.firstElementChild.firstElementChild;
+// a.firstElementChild.style.color = 'blue';
+
+// let ul = document.getElementById('ul');
+// ul.firstElementChild.style.background = 'red';
+let rows = 5;
+let a = document.table.rows;
+console.log(a);
