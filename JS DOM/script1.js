4b3e8153fa1bc4ccf6ace6e8733a65f7a34eb7c8
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-05-31 23:20:23 +0500

    Dom

---
+++ JS DOM/script1.js
@@ -0,0 +1,7 @@
+// let a = document.body.firstElementChild;
+// a.firstElementChild.style.color = 'green';
+// a.lastElementChild.style.color = 'green';
+
+document.querySelectorAll(".box").forEach(e => {
+    e.style.color = "green";
+})
