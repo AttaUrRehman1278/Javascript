94c8a9a5a35cf69a66878469c599afd7655139c4
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-01 20:39:21 +0500

    Exercise12

--- Exercise 12/script.js
+++ Exercise 12/script.js
@@ -1,5 +1,9 @@
-let randomNumber = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
 for(let i = 1; i <= 5; i++){
+    let randomNumber = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+    let randomNumber1 = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
     document.getElementById(`box${i}`).style.color = randomNumber ;
-    document.getElementById(`box${i}`).style.backgroundColor = randomNumber ;
+    document.getElementById(`box${i}`).style.backgroundColor = randomNumber1 ;
+    document.write(randomNumber)
+
 }
+
