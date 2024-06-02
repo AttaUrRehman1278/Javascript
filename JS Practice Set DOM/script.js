37d77a02f8f88d3b26bb8b9809cf86b15c736e4f
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-02 15:09:43 +0500

    Practice Set

--- JS Practice Set DOM/script.js
+++ JS Practice Set DOM/script.js
@@ -1,4 +1,9 @@
-function showAlert(){
-    // alert("Thanks for clicking!")
-    location.replace("https://web.whatsapp.com")
+// function showAlert(){
+//     // alert("Thanks for clicking!")
+//     location.replace("https://web.whatsapp.com")
+// }
+document.getElementById("myBtn").addEventListener("click",yourBookmark);
+
+function yourBookmark(){
+    alert("Thank you! Your bookmark has been saved!")
 }
