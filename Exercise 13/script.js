62a9fbe33321a0b04cd998459cc29eb0f35090bb
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-02 23:33:38 +0500

    card

--- Exercise 13/script.js
+++ Exercise 13/script.js
@@ -5,7 +5,7 @@ function createCard(title, cName, views, monthsOld, duration, thumbnail){
     thumbnail.src = url;
     thumbnail = cards.appendChild(thumbnail);
     title = cards.append("Sigma Web Development | Tutorial # 73");
-    cName = cards.append("\nCode With Harry");
+    cName = cards.append("Code With Harry");
     views = cards.append("20k");
     monthsOld = cards.append("2 months");
     duration = cards.append("31:30");
