11479b65db92583f6d291e4e3807eb981b81df2d
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-05 23:38:02 +0500

    practice

--- YDK JS Practice Problems/script.js
+++ YDK JS Practice Problems/script.js
@@ -3,3 +3,12 @@ let bank_balance = 1000;
 let phone_price = 100;
 let accessories = 10;
 
+while(true){
+    if (purchase <= 900){
+        purchase = purchase + phone_price + accessories;
+    }
+    else{
+        break;
+    }
+}
+console.log(purchase)
