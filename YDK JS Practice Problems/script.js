c9cf075b8eebd5358724cdbb28effb332f6d269b
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-06 00:20:31 +0500

    practice

--- YDK JS Practice Problems/script.js
+++ YDK JS Practice Problems/script.js
@@ -1,23 +1,31 @@
 const SPENDING_THRESHOLD = 200;
-const TAX_RATE = 0.2;
-const PHONE_PRICE = 99.99;
-const PHONE_ACCESSORIES = 9.9;
+const TAX_RATE = 0.02;
+const PHONE_PRICE = 89.99;
+const PHONE_ACCESSORIES = 5.9;
 
-var bank_balance = 1000;
+var bank_balance = 1300;
 var amount = 0;
 
-function calculateTaxRate() {
+function calculateTaxRate(amount) {
     return amount * TAX_RATE;
 }
 
-function formattedAmount() {
-    return "$" + toString(amount);
+function formattedAmount(amount) {
+    return "$" + amount.toFixed(2);
 }
 
-function calculatePurchase() {
+while (amount < bank_balance) {
     amount = amount + PHONE_PRICE;
-    if (amount < SPENDING_THRESHOLD){
-        amount = amount + PHONE_ACCESSORIES;
+    
+    if(amount < SPENDING_THRESHOLD) {
+        amount = amount + PHONE_ACCESSORIES; 
     }
-    return amount;
+}
+
+amount = amount + calculateTaxRate(amount);
+
+console.log(`Your purchase: ${formattedAmount(amount)}`)
+
+if (amount > bank_balance) {
+    console.log("Sorry! you can\'t afford this")
 }
