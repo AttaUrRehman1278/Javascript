abdef400652fc904ebf98830939733f3a715881b
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-07 22:10:46 +0500

    Callbacks

--- JS callbacks and promises/script.js
+++ JS callbacks and promises/script.js
@@ -1,1 +1,7 @@
-alert("Hello world");
+console.log("Hey this is Atta");
+console.log("This is myself");
+
+setTimeout(e => {
+    console.log("Hey this is Atta Ur Rehman")
+}, 0);
+console.log("This is the End of Script")
