62514ee1154856d7cfdc8870cead072a75ff1c42
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-07 22:42:17 +0500

    Callbacks

--- JS callbacks and promises/script.js
+++ JS callbacks and promises/script.js
@@ -1,7 +1,17 @@
-console.log("Hey this is Atta");
-console.log("This is myself");
+// console.log("Hey this is Atta");
+// console.log("This is myself");
 
-setTimeout(e => {
-    console.log("Hey this is Atta Ur Rehman")
-}, 0);
-console.log("This is the End of Script")
+// setTimeout(e => {
+//     console.log("Hey this is Atta Ur Rehman")
+// }, 0);
+// console.log("This is the End of Script")
+function loadScript(src, callback){
+    let script = document.createElement("script");
+    script.src = src;
+    script.onload = callback("Atta Ur Rehman")
+    document.head.append(script)
+}
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js" , (script) =>{
+    alert("script is loaded")
+    alert("script.src")
+})
