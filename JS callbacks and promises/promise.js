b56533ed5e74f2fb4db2dc9fca65e27c0cb7e3c8
AttaUrRehman <atta.shafiq1278@gmail.com>
2024-06-08 23:02:31 +0500

    promises

--- JS callbacks and promises/promise.js
+++ JS callbacks and promises/promise.js
@@ -19,3 +19,25 @@ prom1.then((a) => {
 }).catch((err)=> {
     console.log(err)
 });
+
+function loadScript(src, callback){
+    let script = document.createElement("script");
+    script.src = src;
+    script.onload = callback("Atta Ur Rehman", fn)
+    document.head.append(script)
+}
+// loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js" , (script) =>{
+//     alert("script is loaded")
+//     alert("script.src")
+// })
+
+const fn = ()=>{
+    console.log("Nothing!")
+}
+
+const callback = (arg, fn) =>{
+    console.log(arg)
+    fn()
+}
+
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback)
